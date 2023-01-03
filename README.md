55185fb758bb7a733cb26b5c360c0a1f340b4c07
davidegaspar <git@davidegaspar.com>
2023-01-03 21:38:28 +0000

    wip

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # cli-starter
+
 CLI Starter
+
+## Local development
+
+```sh
+npm install
+npm link
+dx
+```
+
+## Usage
+
+```sh
+# install
+# npm install -g dx
+dx
+```
---
+++ index.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+"use strict";
+
+import {} from "./lib/dotenv.js";
+import { Command } from "commander";
+import boxen from "boxen";
+
+import { logger } from "./lib/logger.js";
+import { getJson } from "./lib/package.js";
+import boxenConfig from "./lib/boxenConfig.js";
+// import setupGithubEnvs from "./commands/setupGithubEnvs/index.js";
+// import setupAppInfra from "./commands/setupAppInfra/index.js";
+// import setupAppCode from "./commands/setupAppCode/index.js";
+// import setupCrossAccountRole from "./commands/setupCrossAccountRoles/index.js";
+// import info from "./commands/info/index.js";
+
+const program = new Command();
+
+program.showHelpAfterError();
+
+program
+  .version(getJson().version, "-v, --version", "output the version")
+  .option("-d, --debug", "print debug logs")
+  .description("DX CLI");
+
+program.on("option:debug", function () {
+  if (this.opts().debug) {
+    logger.level = "debug";
+  }
+});
+
+// program
+//   .command("setup-app-infra")
+//   .description("Setup base application infrastructure")
+//   .option("-o, --outputDir <directory>", "output directory", ".")
+//   .action((options) => {
+//     console.log(boxen("Setup Application Infrastructure", boxenConfig));
+//     setupAppInfra(options);
+//   });
+
+program
+  .command("info")
+  .description("Get information about the current application")
+  .option(
+    "-p, --prompts",
+    "Force information prompts instead of reading from the current environment"
+  )
+  .action((options) => {
+    console.log(boxen("Application Information", boxenConfig));
+    info(options);
+  });
+
+program.parse(process.argv);
