a375cababd3b763db04e7b47ab20f684970d4c23
davidegaspar <git@davidegaspar.com>
2023-01-22 18:20:55 +0000

    add rendering examples

--- config/accounts.js
+++ config/accounts.js
@@ -2,23 +2,37 @@ export default {
   dev: {
     id: "00000",
     regions: {
-      "eu-west-1": {},
+      "eu-west-1": {
+        secret: "aaa",
+      },
     },
   },
   stage: {
     id: "11111",
     regions: {
-      "eu-central-1": {},
-      "us-east-2": {},
-      "ap-northeast-1": {},
+      "eu-central-1": {
+        secret: "bbb",
+      },
+      "us-east-2": {
+        secret: "ccc",
+      },
+      "ap-northeast-1": {
+        secret: "ddd",
+      },
     },
   },
   prod: {
     id: "22222",
     regions: {
-      "eu-central-1": {},
-      "us-east-2": {},
-      "ap-northeast-1": {},
+      "eu-central-1": {
+        secret: "eee",
+      },
+      "us-east-2": {
+        secret: "fff",
+      },
+      "ap-northeast-1": {
+        secret: "ggg",
+      },
     },
   },
 };
